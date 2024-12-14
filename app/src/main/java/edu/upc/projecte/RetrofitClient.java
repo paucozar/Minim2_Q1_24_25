cfd6c0307619ba1ed30321b464140a637bb71099
alexlillo <alexlillo26@gmail.com>
2024-12-14 13:30:59 +0100

    URL actualitzada amb la BBDD

--- app/src/main/java/edu/upc/projecte/RetrofitClient.java
+++ app/src/main/java/edu/upc/projecte/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://10.0.2.2:8080/dsaApp/"; // Substitueix amb el teu URL base
+    private static final String BASE_URL = "http://10.0.2.2:8080/dsaApp/"; // Per tenir connexió amb la BBDD cal tenir posada la URL: http://10.0.2.2:8080/dsaApp/
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
